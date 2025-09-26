e59fd4d7ff5deb6ef2f7c0fe3b07004ff38561a7
Ravikumar1810 <nkravikumar18@gmail.com>
2025-09-26 21:27:53 +0530

    App setup

---
+++ wether-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='text-blue-800 text-3xl bg-red-100 font-bold underline center m-10'>
+          Hello world!
+        </h1>
+        <p className='text-2xl font-bold color:red-100 center'>Good to see you here :) </p>
+      </div>
+    </>
+  )
+}
+
+export default App
